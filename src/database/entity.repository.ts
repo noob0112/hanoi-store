1d0f030edbd8f9598fd2595fa287b6153e485005
Nguyen Viet Hoang <hoang011220@gmail.com>
2022-07-15 11:07:33 +0700

    update

--- src/database/entity.repository.ts
+++ src/database/entity.repository.ts
@@ -4,6 +4,7 @@ import {
   Model,
   UpdateQuery,
   QueryOptions,
+  // ClientSession,
 } from 'mongoose';
 import { objectId } from 'src/common/types';
 
@@ -58,9 +59,11 @@ export abstract class EntityRepository<T extends Document> {
     updateEntitydata: UpdateQuery<unknown>,
     options: QueryOptions<T> = { new: true },
   ): Promise<T | unknown | null> {
-    return await this.entityModel
-      .updateOne(entityFilterQuery, updateEntitydata, options)
-      .exec();
+    return await this.entityModel.updateOne(
+      entityFilterQuery,
+      updateEntitydata,
+      options,
+    );
   }
 
   findOneAndUpdate(
@@ -70,7 +73,9 @@ export abstract class EntityRepository<T extends Document> {
   ): Promise<T | null> {
     return this.entityModel
       .findOneAndUpdate(entityFilterQuery, updateEntitydata, options)
-      .exec();
+      .then((doc) => {
+        return doc;
+      });
   }
 
   async findByIdAndUpdate(
@@ -78,29 +83,29 @@ export abstract class EntityRepository<T extends Document> {
     updateEntitydata: UpdateQuery<unknown>,
     options: QueryOptions<T> = { new: true },
   ): Promise<T | null> {
-    return this.entityModel.findByIdAndUpdate(id, updateEntitydata, options);
+    return await this.entityModel.findByIdAndUpdate(
+      id,
+      updateEntitydata,
+      options,
+    );
   }
 
   async findOneAndDelete(
     entityFilterQuery: FilterQuery<T>,
     entityOption?: QueryOptions<T>,
   ): Promise<T | null> {
-    return this.entityModel
-      .findOneAndDelete(entityFilterQuery, entityOption)
-      .exec();
+    return this.entityModel.findOneAndDelete(entityFilterQuery, entityOption);
   }
 
   async findByIdAndDelete(
     id: objectId | string,
     entityOption?: QueryOptions<T>,
   ): Promise<T | null> {
-    return this.entityModel.findByIdAndDelete(id, entityOption).exec();
+    return this.entityModel.findByIdAndDelete(id, entityOption);
   }
 
   async deleteMany(entityFilterQuery: FilterQuery<T>): Promise<boolean> {
-    const deleteResult = await this.entityModel
-      .deleteMany(entityFilterQuery)
-      .exec();
+    const deleteResult = await this.entityModel.deleteMany(entityFilterQuery);
     return deleteResult.deletedCount >= 1;
   }
 }
