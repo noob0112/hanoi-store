1db9718332baaee07985e57abe3e49d8c8b15c4b
Nguyen Viet Hoang <hoang011220@gmail.com>
2022-07-17 14:21:33 +0700

    add query param dto

--- src/modules/vouchers/dtos/query-voucher.dto.ts
+++ src/modules/vouchers/dtos/query-voucher.dto.ts
@@ -1,11 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Type } from 'class-transformer';
 import { IsInt, IsOptional, Min } from 'class-validator';
 
 export class QueryVoucherDto {
   @ApiProperty({ required: false })
   @IsOptional()
-  @Type(() => Number)
   @IsInt()
   @Min(1)
   page?: number;
